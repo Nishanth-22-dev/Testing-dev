5d336f267545979f09479a2776b38f4217448989
Nishanth-22-dev <nishanthsrirajaa@gmail.com>
2024-08-06 16:13:41 +0530

    added routes and folders

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("home.html")
+
 
 @app.route('/desserts')
 def routing():
